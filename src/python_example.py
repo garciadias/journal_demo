66ab9ad979b705a691a5e46d8873016257440603
R. Garcia-Dias <rafaelgd@iac.es>
2017-12-14 16:17:02 +0000

    Replace pairplot with KDEplot

--- src/python_example.py
+++ src/python_example.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import os
 
 rep_path = os.getcwd()
-dset = pd.read_csv("%s/data/m67_photometry.csv" % rep_path, index_col=0)
+dset = pd.read_csv("%s/data/m67_photometry.csv" % rep_path,
+                   index_col=0)
 
-sns.pairplot(dset, x_vars=['JK'], y_vars=['J'],
-             hue='survey', size=9, plot_kws=dict(s=65))
-
-plt.ylim(18, 5)
+fig = plt.figure(figsize=(9, 9))
+ax = fig.add_subplot(111)
+sns.kdeplot(dset.JK, dset.J, shade=True)
+plt.ylim(18, 10)
 plt.xlim(-0.15, 2)
+plt.title('M67 2MASS', fontsize=18)
 plt.show()
