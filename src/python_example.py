52ba072faf33e68f7ed8b5c8f31b2b621bacc701
R. Garcia-Dias <rafaelgd@iac.es>
2017-12-14 16:31:31 +0000

    Add title to pairplot

--- src/python_example.py
+++ src/python_example.py
@@ -8,7 +8,7 @@ dset = pd.read_csv("%s/data/m67_photometry.csv" % rep_path, index_col=0)
 
 sns.pairplot(dset, x_vars=['JK'], y_vars=['J'],
              hue='survey', size=9, plot_kws=dict(s=65))
-
+plt.title('2MASS Photometry')
 plt.ylim(18, 5)
 plt.xlim(-0.15, 2)
 plt.show()
